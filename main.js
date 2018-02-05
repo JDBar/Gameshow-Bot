0f001418e51406084bd14f03826f2bd2db83e901
JDBar <jdbar1215@gmail.com>
2018-02-05 02:36:25 -0800

    :new: Added bot token.

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = "NDEwMDE0MzUxMzgzMDY4Njcy.DVm_QQ.9NiEie04wiR58VWChZdaGeea4KE";
 
-console.log("Lights... camera... action!");
+client.on(`ready`, function () {
+  console.log(`Lights... camera... action!`);
+})
+
+client.on(`message`, function (message) {
+  if (message.author.id === message.guild.ownerID) {
+    switch (message.content) {
+      case "ping!":
+        message.channel.send("pong!");
+        break;
+    }
+  }
+})
+
+client.login(token);
